d282b66d99045ecaabb9b44ed1d868d38615bbca
Nicolas Lopez <unacorbatanegra@gmail.com>
2022-01-07 11:44:27 -0300

    isCryptSolution

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -1,5 +1,7 @@
 package main
 
+import "strconv"
+
 /* Problem:
 Given an array a that contains only numbers in the range from 1 to a.length,
 find the first duplicate number for which the second occurrence has the minimal index.
@@ -132,4 +134,52 @@ func sudoku(grid [][]string) bool {
 
 	return false
 
+}
+
+/* Problem:
+
+A cryptarithm is a mathematical puzzle for which the goal is to find
+the correspondence between letters and digits, such that the given
+arithmetic equation consisting of letters holds true
+when the letters are converted to digits.
+You have an array of strings crypt, the cryptarithm,
+and an an array containing the mapping of letters and digits, solution.
+The array crypt will contain three non-empty strings
+that follow the structure: [word1, word2, word3], which should
+be interpreted as the word1 + word2 = word3 cryptarithm.
+If crypt, when it is decoded by replacing all of the letters in
+the cryptarithm with digits using the mapping in solution,
+becomes a valid arithmetic equation containing no numbers with leading zeroes,
+the answer is true. If it does not become a valid arithmetic solution, the answer is false.
+Note that number 0 doesn't contain leading zeroes (while for example 00 or 0123 do).
+*/
+
+func isCryptSolution(crypt []string, solution [][]string) bool {
+	m := make(map[string]string)
+
+	for i, v := range solution {
+		m[v[0]] = solution[i][1]
+	}
+	list := [3]int{}
+
+	for i, v := range crypt {
+		var s string
+		if i == 0 && m[string(v[0])] == "0" && len(v) > 1 {
+			return false
+		}
+		for c, l := range v {
+			if c == 0 && m[string(l)] == "0" && len(v) > 1 {
+				return false
+			}
+			s += m[string(l)]
+		}
+		tmp, err := strconv.Atoi(s)
+		if err != nil {
+			return false
+		}
+		list[i] = tmp
+
+	}
+
+	return list[0]+list[1] == list[2]
 }
